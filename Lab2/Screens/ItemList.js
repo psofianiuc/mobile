6bfd9485b638e6f6251e9bce071f06e925e029c2
Paula Sofianiuc <paula.sofianiuc@endava.com>
2017-12-08 09:19:34 +0200

    Delete all

--- Lab2/Screens/ItemList.js
+++ Lab2/Screens/ItemList.js
@@ -2,7 +2,7 @@
  * Created by admin on 05/12/2017.
  */
 import React, {Component} from 'react';
-import {Text, View, StyleSheet, ListView, AsyncStorage } from 'react-native';
+import {Text, View, StyleSheet, ListView, AsyncStorage, Button } from 'react-native';
 
 
 export default class CatList extends Component {
@@ -46,22 +46,48 @@ export default class CatList extends Component {
 
     render() {
         return (
+        <View>
+            <View style={styles.buttonContainer}>
+                <Button onPress={this._deleteAll.bind(this)} title="Delete all" color="#621515"/>
+            </View>
             <ListView
                 dataSource={this.state.dataSource}
                 renderRow={(data) => this.renderRow(data)}
             />
+        </View>
         );
     }
 
     renderRow = (data) => {
         console.log(data);
+        var str = "";
+        for (obj in data) {
+            str += " " + data[obj];
+        }
         return (
             <View style={styles.rowStyle}>
-                <Text style={styles.rowTextStyle}>{data[0]} - {data[1]}</Text>
+                <Text style={styles.rowTextStyle}>{str}</Text>
             </View>
-        );
-     }
 
+        );
+     };
+
+    _deleteAll() {
+        console.log("Delete all");
+        try {
+            Promise.all(AsyncStorage.getAllKeys()
+                .then(ks => {
+                    ks.map(k => {
+                            AsyncStorage.removeItem(k);
+                            this.setState({items:[]});
+                            this.forceUpdate();
+                    })
+                })
+            );
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
 }
 
 var styles = StyleSheet.create({
@@ -87,5 +113,23 @@ var styles = StyleSheet.create({
     rowTextStyle: {
         color: '#621515',
         fontSize: 14,
-    }
+    },
+    buttonContainer: {
+        marginTop: 10,
+        backgroundColor: '#ffffff',
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 120,
+        height: 45,
+        borderColor: '#621515',
+        borderWidth: 2,
+        borderRadius: 4,
+        shadowColor: '#000000',
+        shadowOffset: {
+            width: 0,
+            height: 3
+        },
+        shadowRadius: 10,
+        shadowOpacity: 0.25
+    },
 });
